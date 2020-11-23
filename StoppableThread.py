8f0a0ce5ec41bf00dc3fc6792829be9e1dd0410b
Wim Nelis <wim.nelis@ziggo.nl>
2020-11-23 13:41:52 +0100

    Modules: Add non-standard modules needed by script mqtt.rfxcom.py.

---
+++ StoppableThread.py
@@ -0,0 +1,96 @@
+#
+# Class StoppableThread extends class threading.Thread with two options
+# regarding stopping:
+# A) Event _stop_event is added to the thread as well as methods to set and
+#    check this event. The thread itself has to check regularly for the
+#    stopped() condition.
+# B) A call-back can be specified which is invoked at normal and abnormal
+#    termination of the thread. This is incorporated to prevent the main thread
+#    controller from polling the status of the thread(s) regularly.
+#
+# The main loop is normally put in method run(), but this method is already
+# defined in this class and should not be redefined in a derived class. In stead
+# a derived class should define a method named loop() containing the main loop.
+#
+# The call-back function is invoked with two parameters, the name of the thread
+# and in case of an abnormal termination the description of the exception. In
+# case of a normal termination, the second parameter is None.
+#
+# Moreover, method Wait (an extended version of time.sleep) is added, as it is
+# needed by many subclasses. It is meant for cases in which the thread has to
+# wait some fixed time but react fast when event _stop_event is set.
+#
+# This class is intended for use cases in which (A) the threads are to be active
+# for extended periods of time, typically multiple days, and (B) the termination
+# of one thread should cause all other threads, including the main thread, to
+# stop too.
+#
+# NOTE: threading.excepthook can also handle the (uncaught) exceptions occurring
+#       in Thread.run. However, it cannot handle the normal termination of a
+#       thread.
+#
+import time
+import threading
+
+class StoppableThread( threading.Thread ):
+  """Thread class with a stop() method and a call-back invoked at termination."""
+
+  def __init__( self, callback=None, *args, **kwargs ):
+    super().__init__( *args, **kwargs )
+    self._stop_event  = threading.Event()
+    self._on_terminate= callback
+
+  def join( self, TimeOut=None ):
+    self.stop()
+    super().join( TimeOut )
+
+ #
+ # Abstract method loop is invoked by method StoppableThread.run. The
+ # implementation below should be overridden in any derived class.
+ #
+  def loop( self ):
+    raise NotImplementedError
+
+ #
+ # Method run is invoked by threading.Thread.start. This method invokes the
+ # overridden method loop to do the actual work. Upon termination of method
+ # loop, it will invoke the callback function.
+ #
+  def run( self ):
+    try:
+      self.loop()
+    except Exception as e:
+      if self._on_terminate is not None:
+        self._on_terminate( self.name, str(e) )
+    else:
+      if self._on_terminate is not None:
+        self._on_terminate( self.name, None )
+
+  def stop( self ):
+    self._stop_event.set()
+
+  def stopped( self ):
+    return self._stop_event.is_set()
+
+  def wait( self, timeout ):
+    return self._stop_event.wait( timeout )
+
+ #
+ # Method Wait waits until the (Unix) timestamp reaches the next integer
+ # multiple of Period seconds and then another Delay seconds. However, if the
+ # thread method stop is invoked before the time has expired, this method will
+ # return at that time.
+ # Parameters Period and Delay are integer numbers, with Period >= 2 and
+ # 0 <= Delay < Period.
+ #
+  def Wait( self, Period, Delay ):
+    assert Period >=    2 , 'Period is too small'
+    assert Delay  >=    0 , 'Negative delays are not allowed'
+    assert Delay  < Period, 'Delay too large'
+    Now= time.time()
+    ActTim= int( Now )
+    ActTim= ( (ActTim+Period-1) // Period ) * Period
+    SlpTim= int( ActTim - Now ) + Delay
+    if SlpTim < 1.5:  SlpTim+= Period
+    self.wait( SlpTim )
+
---
+++ bfsm.py
@@ -0,0 +1,173 @@
+#
+# Definition of an as simple as possible Finite State Machine.
+#
+# A basic FSM (BFSM) consists of a list of states, a list of stimuli and a
+# matrix (dictionary) with one entry per (state,stimulus)-pair specifying the
+# new state and the action to be performed. The list of states and the list of
+# stimuli are build from the supplied matrix.
+#
+# Stimuli can originate from a source external to the BFSM or from the BFSM
+# itself. In principle stimuli from both sources are entered into the same queue
+# (FIFO) within the BFSM, and are retrieved once the BFSM is ready to handle the
+# next stimulus. Some internal stimuli are in fact modifiers on the stimulus
+# being handled: these are passed via the high-priority queue.
+#
+# In addition to event actions, there are state actions. A state action depends
+# only on the state. These are performed after entering a state and just before
+# the event action is performed. If a state action returns a True value, the
+# event action will be invoked too, but if it returns a False value, the event
+# action will be skipped.
+#
+# If this FSM will be part of an object, in which case the actions will be
+# methods of the embedding object, the references to the actions should be
+# 'bound method references'. This can be enforced using function
+# types.MethodType.
+#
+# Version 0.00, W.J.M. Nelis, wim.nelis@ziggo.nl, 2018.01
+#
+# Version 0.01, W.J.M. Nelis, wim.nelis@ziggo.nl, 2018.12
+# - Stop experiment with asyncio. This implies that the FSM is no longer a long
+#   running thread, which blocks until a stimulus arrives, but becomes a
+#   function which is invoked to handle one or a few stimuli. It returns control
+#   to the caller once done.
+# - Build the list of states and the list of stimuli from the matrix.
+# - Add comment sections.
+#
+# Version 0.02, W.J.M. Nelis, wim.nelis@ziggo.nl, 2019.01
+# - Add state actions in addition to the event actions. A state action depends
+#   only on the state. These are performed after entering a state and just
+#   before the event action is performed. If a state action returns a True
+#   value, the event action will be invoked too, but if it returns a False
+#   value, the event action will be skipped.
+# - For convenience a single watchdog-timer, named wdt, is created together with
+#   the FSM. It is not used within this class.
+#
+# Version 0.03, W.J.M. Nelis, wim.nelis@ziggo.nl, 2020.02
+# - Methods ReportEvent and HandleEvent accept an optional parameter. This
+#   parameter is accessible in instance variable self.Parametr when the
+#   associated action is performed.
+#
+# Version 0.04, W.J.M. Nelis, wim.nelis@ziggo.nl, 2020.05
+# - Remove class WatchdogTimer. It is put into a separate module.
+# - If an action is in fact a method, it is up to the caller to make sure that
+#   those actions are 'bound method references'. The poor man's solution to pass
+#   an additional parameter with the value of self is removed.
+#
+import queue
+
+_debug_fsm_= True			# Control debug output
+
+class Bfsm:
+  '''A very basic Finite State Machine'''
+
+  def __init__( self, Matrix, StaAct=None ):
+    self.Matrix= Matrix			# Decision table of the FSM
+    self.StaAct= StaAct			# State pre- and post-action vector
+
+   # Build a list of all possible states and a list of all possible stimuli, by
+   # reading the keys of matrix Matrix.
+    self.States = []			# Preset list of states
+    self.Stimuli= []			# Preset list of stimuli
+    for State in Matrix:
+      self.States.append( State )
+      for Stim in Matrix[State]:
+        if not Stim in self.Stimuli:
+          self.Stimuli.append( Stim )
+
+   # Check the matrix to be complete, and see if the new state in each entry
+   # does exist.
+    for State in self.States:
+#     assert State in Matrix		# This assertion should never fail
+      for Stim in self.Stimuli:
+        assert Stim in Matrix[State]
+        assert Matrix[State][Stim][0] in self.States
+
+   # If a list of state actions is specified, make sure it contains an entry for
+   # each state.
+    if StaAct is not None:
+      for State in self.States:
+        self.StaAct.setdefault( State, None )
+      for State in StaAct:
+        assert State in self.States
+
+   # Define the queues to pass the stimuli, both with normal and with high
+   # priority. Preset the object variables.
+    self.DefQueue= queue.Queue( 16 )
+    self.PriQueue= queue.Queue(  2 )
+    self.State   = 'Init'		# Current state
+    self.PrvState= None			# Previous state
+    self.NxtState= None			# Next state
+    self.Stimulus= None			# Event
+    self.Parametr= None			# Event parameter
+
+ # Private method _Interpret is the interpreter of the BFSM. It fetches the next
+ # stimulus from either the high-prior queue or the default queue, determines
+ # it's new state and invokes the associated action. If the stimuli queues are
+ # empty the interpreter stops, that is it returns control to it's caller.
+  def _Interpret( self ):
+    trace= []				# Trace of {state,stimulus} history
+    while ( 1 ):
+      Stim= None
+      if   not self.PriQueue.empty():
+        Stim=  self.PriQueue.get()
+      elif not self.DefQueue.empty():
+        Stim=  self.DefQueue.get()
+      if Stim is None:
+        return trace
+
+      if type(Stim) is tuple:
+        (Stim,self.Parametr)= Stim[:]
+      elif type(Stim) is str:
+        self.Parametr= None
+      else:
+        assert False, 'Unexpected event type'
+
+      if _debug_fsm_:
+        trace.append( "State: {}, Stim: {}, Par: {}".format(
+                      self.State, Stim, self.Parametr ) )
+      assert self.Stimuli.count( Stim ) == 1
+      (NewState,Action)= self.Matrix[self.State][Stim][:]
+#     assert self.States.count( NewState ) == 1
+
+      self.Stimulus= Stim
+      if self.StaAct is not None:	# Do state action
+        if self.StaAct[self.State] is not None:
+          self.NxtState= NewState
+          if not self.StaAct[self.State]():
+            continue
+      if self.Parametr is None:
+        Action()
+      else:
+        Action( self.Parametr )
+
+      self.PrvState= self.State
+      self.State   = NewState
+      self.NxtState= None
+
+ # Method ReportEvent enters the supplied stimulus in the default queue and
+ # returns. The stimulus will be handled when method HandleEvent is called.
+  def ReportEvent( self, Stim, Par=None ):
+    assert self.Stimuli.count( Stim ) == 1
+    if Par is None:
+      self.DefQueue.put( Stim )
+    else:
+      self.DefQueue.put( (Stim,Par) )
+
+ # Method AugmentEvent enters the supplied stimulus in the high priority queue.
+ # Typically, this method is called from an action invoked by this FSM, causing
+ # this stimulus to be handled directly upon return from the action.
+  def AugmentEvent( self, Stim ):
+    assert self.Stimuli.count( Stim ) == 1
+    self.PriQueue.put( Stim )
+
+ # Method HandleEvent enters the supplied stimulus in the default queue and
+ # invokes the FSM interpreter to handle the stimulus.
+  def HandleEvent( self, Stim, Par=None ):
+    self.ReportEvent( Stim, Par )
+    return self._Interpret()
+
+ # Method GetState retrieves the current state and the last stimulus. Note that
+ # the state will not change until the action associated with the aforementioned
+ # pair (State,Stimulus) is completed.
+  def GetState( self ):
+    return (self.State,self.Stimulus)
